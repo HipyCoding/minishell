13de4dd98cf48b81943a6430d59474530bd23223
Candrese <candrese@student.42Heilbronn.de>
2024-11-02 08:34:32 +0100

    moved lexer function to this file

--- parsing/init_lexer.c
+++ parsing/init_lexer.c
@@ -6,7 +6,7 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 05:02:23 by candrese          #+#    #+#             */
-/*   Updated: 2024/10/31 06:01:05 by candrese         ###   ########.fr       */
+/*   Updated: 2024/11/02 08:33:46 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,4 +64,28 @@ t_token *init_new_token(const char *input, int *i, t_token *prev_token)
 		return NULL;
 	}
 	return new_token;
+}
+
+// Main lexer function
+t_token *lexer(const char *input)
+{
+	t_token *head;
+	t_token *new_token;
+	t_token *prev_token;
+	int i;
+
+	head = NULL;
+	prev_token = NULL;
+	i = 0;
+
+	while (skip_whitespace(input, &i))
+	{
+		new_token = init_new_token(input, &i, prev_token);
+		if (!new_token)
+			return NULL;
+		add_token(&head, new_token);
+		prev_token = new_token;
+		i++;
+	}
+	return head;
 }
