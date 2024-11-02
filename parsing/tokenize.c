04340090853c93a8c645236accbd4037c6b618f2
Candrese <candrese@student.42Heilbronn.de>
2024-11-02 08:46:52 +0100

    updated get_basic_token_type() and get_token_type() to correctly handle expansion and moved them to this file. (previously thez were in parse_utils.c)

--- parsing/tokenize.c
+++ parsing/tokenize.c
@@ -12,6 +12,51 @@
 
 # include "../minishell.h"
 
+// get_basic_token_type
+ast_type get_basic_token_type(char first_char)
+{
+	if (first_char == '|')
+		return NODE_PIPE;
+	else if (first_char == '>' || first_char == '<')
+		return NODE_REDIR;
+	else if (first_char == '$')
+		return NODE_ENV;
+	else if (first_char == '"' || first_char == '\'')
+		return NODE_QUOTE;
+	else
+		return NODE_WORD;
+}
+
+// get_token_type to maybe change WORD NODE to CMD or ARG
+ast_type get_token_type(const char *data, const t_token *prev_token)
+{
+	ast_type basic_type;
+
+	// Check for environment variable
+	if (data[0] == '$')
+	{
+		// If it's after a pipe or at the start, it should be a command
+		if (!prev_token || prev_token->type == NODE_PIPE)
+			return NODE_CMD;
+		// Otherwise it's an argument
+		return NODE_ARG;
+	}
+	basic_type = get_basic_token_type(data[0]);
+	// If it's already a special token, return that type
+	if (basic_type != NODE_WORD)
+		return basic_type;
+	// If no previous token, or previous token was a pipe, this word is a command
+	if (!prev_token || prev_token->type == NODE_PIPE)
+		return NODE_CMD;
+	// If previous token was a redirection, this is a filename (WORD)
+	if (prev_token->type == NODE_REDIR)
+		return NODE_WORD;
+	// If previous token was a command or argument, this is an argument
+	if (prev_token->type == NODE_CMD || prev_token->type == NODE_ARG)
+		return NODE_ARG;
+	return NODE_WORD;
+}
+
 // Add token to list
 void add_token(t_token **head, t_token *new_token)
 {
@@ -26,30 +71,6 @@ void add_token(t_token **head, t_token *new_token)
 	while (current->next)
 		current = current->next;
 	current->next = new_token;
-}
-
-// Main lexer function
-t_token *lexer(const char *input)
-{
-	t_token *head;
-	t_token *new_token;
-	t_token *prev_token;
-	int i;
-
-	head = NULL;
-	prev_token = NULL;
-	i = 0;
-
-	while (skip_whitespace(input, &i))
-	{
-		new_token = init_new_token(input, &i, prev_token);
-		if (!new_token)
-			return NULL;
-		add_token(&head, new_token);
-		prev_token = new_token;
-		i++;
-	}
-	return head;
 }
 
 // for Debug/Print
