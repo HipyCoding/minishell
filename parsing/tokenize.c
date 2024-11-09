a17ddeb87d8ee147d83fdd00ce493ff5c4daf7be
Candrese <candrese@student.42Heilbronn.de>
2024-11-10 00:02:06 +0100

    improved print function for debugging, it shows the content of the node-> data now

--- parsing/tokenize.c
+++ parsing/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:01:49 by candrese          #+#    #+#             */
-/*   Updated: 2024/11/08 14:01:59 by candrese         ###   ########.fr       */
+/*   Updated: 2024/11/09 17:41:51 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,7 @@ void print_tokens(t_token *head)
 	printf("\n");
 }
 
+
 // for Debug/Print
 void print_ast(t_ast_node *node, int depth)
 {
@@ -96,22 +97,40 @@ void print_ast(t_ast_node *node, int depth)
 	for (i = 0; i < depth; i++)
 		printf("  ");
 
-	if(node -> type == NODE_PIPE)
+	// Print token type and node data
+	if (node->type == NODE_PIPE)
 		printf("PIPE\n");
-	else if(node -> type == NODE_CMD)
-		printf("CMD\n");
-	else if(node -> type == NODE_REDIR)
-		printf("REDIR\n");
-	else if(node -> type == NODE_ARG)
-		printf("ARG\n");
-	else if(node -> type == NODE_WORD)
-		printf("WORD\n");
-	else if(node -> type == NODE_ENV)
-		printf("ENV\n");
-	else if(node -> type == NODE_QUOTE)
-		printf("QUOTE\n");
-
-	print_ast(node->args, depth + 1);
+	else if (node->type == NODE_CMD)
+		printf("CMD: '%s'\n", node->data);
+	else if (node->type == NODE_REDIR)
+	{
+		printf("REDIR (");
+		if (node->redir_type == 1)
+		printf(">");
+		else if (node->redir_type == 2)
+			printf("<");
+		else if (node->redir_type == 3)
+			printf(">>");
+		else if (node->redir_type == 4)
+			printf("<<");
+		printf(")\n");
+	}
+	else if (node->type == NODE_ARG)
+		printf("ARG: '%s'\n", node->data);
+	else if (node->type == NODE_WORD)
+		printf("WORD: '%s'\n", node->data);
+	else if (node->type == NODE_ENV)
+		printf("ENV: '%s'\n", node->data);
+	else if (node->type == NODE_QUOTE)
+		printf("QUOTE: '%s'\n", node->data);
+	// print children nodes
+	if (node->args)
+	{
+		for (i = 0; i < depth + 1; i++)
+			printf("  ");
+		printf("Arguments:\n");
+		print_ast(node->args, depth + 2);
+	}
 	print_ast(node->left, depth + 1);
 	print_ast(node->right, depth + 1);
 }
